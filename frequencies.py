1d5dd731374a6ea2ff34529ebc279e7793ab5b99
Tlthon <82746063+Tlthon@users.noreply.github.com>
2022-10-13 11:06:53 +0000

    adding base function

--- frequencies.py
+++ frequencies.py
@@ -1,7 +1,9 @@
 """Frequencies function."""
 """ENTER YOUR SOLUTION HERE!"""
 
-def frequencies(items):
-    frequencies = {}
+def frequencies(items :list[any]):
+    freq:dict[any] = {}
+    for item in items:
+        freq[item] = freq.get(item, 0) + 1
     # Your code goes here
-    return frequencies
+    return freq
