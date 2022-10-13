ed790a343803911411cf30187769ff5ec58ef3df
Tlthon <82746063+Tlthon@users.noreply.github.com>
2022-10-13 11:09:17 +0000

    adding string conversion

--- frequencies.py
+++ frequencies.py
@@ -4,6 +4,7 @@
 def frequencies(items :list[any]):
     freq:dict[any] = {}
     for item in items:
-        freq[item] = freq.get(item, 0) + 1
+        str_item = str(item)
+        freq[str_item] = freq.get(str_item, 0) + 1
     # Your code goes here
     return freq
